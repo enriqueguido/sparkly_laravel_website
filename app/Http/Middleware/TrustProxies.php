d0c6522464f1b32a2f8df7b36e99cfd502a39514
Max <enrique.guido@yahoo.com>
2018-12-27 18:43:58 -0500

    Commiting changes to app/http/middleware/trustproxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Http\Middleware;
@@ -7,17 +8,13 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array
-     */
-    protected $proxies;
+    /** 
+    The trusted proxies for this application.
+    */
+    protected $proxies = '*';
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    /** 
+    The headers that should be used to detect proxies.
+    */
+    protected $headers = Request:: HEADER_X_FORWARDED_AWS_ELB;
 }
